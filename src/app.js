539a72918667107261e0dce33dce0a353768085d
unknown <nhl9a1vn@gmail.com>
2022-09-11 14:23:36 +0700

    commit for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -52,8 +53,7 @@ app.get('/weather', (req, res) => {
         if (error) {
             return res.send({ error })
         }
-
-        forecast(latitude, longitude, (error, forecastData) => {
+        else forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
                 return res.send({ error })
             }
@@ -95,6 +95,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on' + port)
 })
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -2,12 +2,15 @@ const request = require('request')
 
 const geocode = (address, callback) => {
     const url = 'https://api.geoapify.com/v1/geocode/search?text='+ encodeURIComponent(address) + '&apiKey=459ba8a8266944d9b1940ce0d1623e4f'
-
+    
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
         } else if (body.features.length === 0) {
-            callback('Unable to find location. Try another search.', undefined)
+            callback('Unable to find location. Try another search.',{
+                latitude: undefined,
+                longitude: undefined
+            })
         } else {
             callback(undefined, {
                 latitude: body.features[0].properties.lat,
